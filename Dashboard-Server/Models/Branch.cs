d99aae48b201bb98abf1df53d6e2af519e0681c4
Carlos Ramirez <carlos.ramv97@gmail.com>
2023-01-29 14:43:09 -0700

    add model of branches

---
+++ Dashboard-Server/Models/Branch.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Dashboard_Server.Models
+{
+    public class Branch
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("name")]
+        public string? name { get; set; }
+        [BsonElement("key")]
+        public string? Key { get; set; }
+        [BsonElement("status")]
+        public bool? Status { get; set; } = true;
+        [BsonElement("created_at")]
+        public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
